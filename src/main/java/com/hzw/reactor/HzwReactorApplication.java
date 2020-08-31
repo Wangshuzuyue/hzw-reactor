219827c54ac86a014e836e0b0c04cea59361545c
Wangshuzuyue <79214429@qq.com>
2020-08-31 19:44:44 +0800

    first commit

---
+++ src/main/java/com/hzw/reactor/HzwReactorApplication.java
@@ -0,0 +1,13 @@
+package com.hzw.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzwReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzwReactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzw/reactor/HzwReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzw.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzwReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
